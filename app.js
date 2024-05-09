13cd7d474091484fac0c8650a233a499cd7cbc34
aman <amandon.pathak12@gmail.com>
2024-05-09 07:14:51 +0530

    fourth comit

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-document.addEventListener("DOMContentLoaded", () => {
+document.addEventListener("DOMContentLoaded", (e) => {
   const navbarContainer2 = document.querySelector(
     ".navbar .navbar-container-2"
   );
@@ -17,7 +17,10 @@ document.addEventListener("DOMContentLoaded", () => {
   const overlay = document.querySelector(".overlay");
   const cross = document.querySelector(".overlay span");
   const searchNav = document.querySelector("#nav_2 .nav-btn label");
-  const hero_section = document.querySelector(".hero-section");
+  const detailsDropdown = document.querySelectorAll("details.dropdown");
+  const navDrop = document.querySelectorAll(
+    'details.dropdown summary + ul[role="nav-drop"]'
+  );
 
   searchNav?.addEventListener("click", () => {
     overlay?.classList.add("activate");
@@ -30,6 +33,17 @@ document.addEventListener("DOMContentLoaded", () => {
   console.log("I AM THE MAIN SCRIPT FILE!");
   console.log(searchNav);
   console.log(cross);
+
+  detailsDropdown.forEach((detailDropd) => {
+    // if (detailDropdown.hasAttribute("open")) {
+    //   detailDropdown.style.color = "orange";
+    // }
+    detailDropd.addEventListener("click", (e) => {
+      detailDropd.classList.toggle("active");
+      // document.querySelector("details.dropdown")?.classList.remove("active");
+      // detailDropd.classList.add("active");
+    });
+  });
 });
 
 // const navbarContainer2 = document.querySelector(".navbar .navbar-container-2");
